ce16020df5e5caefe1ba5802924a12dfc89ceb35
Khushi Jain <khushi.jain0704@gmail.com>
2022-01-09 12:04:09 +0530

    home section

--- js/app.js
+++ js/app.js
@@ -6,4 +6,26 @@ $(document).ready(function(){
         $('#search-box').toggleClass('active')
     });
 
+        $('#menu').click(function(){
+            $(this).toggleClass('fa-times')
+            $('.navbar').toggleClass('nav-toggle')
+            
+        });
+
+        $(window).on('scroll load',function(){
+            
+            $('#search-icon').removeClass('fa-times')
+            $('#search-box').removeClass('active')
+
+            $('#menu').removeClass('fa-times')
+            $('.navbar').removeClass('nav-toggle')
+
+            if($(window).scrollTop() > 0){
+                $('header').addClass('sticky');
+            }else{
+                $('header').removeClass('sticky');
+            }
+
+        });
+        
 });
